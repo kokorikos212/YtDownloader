03e1e8fb53528a4a47347af4047f7e1dc5ad38d1
NospaZspacE <96020112+kokorikos212@users.noreply.github.com>
2023-11-07 20:41:26 +0200

    create_player_database.py

--- create_player_database.py
+++ create_player_database.py
@@ -1,26 +1,49 @@
 import sqlite3
 
-# Replace with your desired table name
-name_table = "mytable"
+# Define the SQLite database file name
+db_file = 'YT_database.db'
 
-# Connect to the SQLite database (it will create a new database if it doesn't exist)
-conn = sqlite3.connect('Downloads_Database.db')
+# Create a connection to the database (or create a new database if it doesn't exist)
+conn = sqlite3.connect(db_file)
 
 # Create a cursor object to execute SQL commands
 cursor = conn.cursor()
 
-# Create the table if it doesn't already exist
-cursor.execute(f'''
-    CREATE TABLE IF NOT EXISTS mytable(
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
+# Create a Songs table
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Songs (
+        song_id INTEGER PRIMARY KEY,
         title TEXT,
         author TEXT,
         views INTEGER,
         length INTEGER,
-        publish_date TEXT
+        publish_date TEXT,
+        link TEXT
     )
 ''')
 
-# Commit the changes and close the connection
+# Create an Artists table
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Artists (
+        artist_id INTEGER PRIMARY KEY,
+        name TEXT
+    )
+''')
+
+# Create the Song-Artist Relationship Table (Many-to-Many)
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS SongArtist (
+        relation_id INTEGER PRIMARY KEY,
+        song_id INTEGER,
+        artist_id INTEGER,
+        FOREIGN KEY (song_id) REFERENCES Songs (song_id),
+        FOREIGN KEY (artist_id) REFERENCES Artists (artist_id)
+    )
+''')
+
+# Commit the changes and close the database connection
 conn.commit()
 conn.close()
+
+print("Database and tables have been created.")
+
